248b7ad83e0690123a9008878b4a70083986d0d4
Rob Law <rob@flywheelnetworks.com>
2014-04-11 13:42:06 -0700

    Update gems and bump version.

--- snack.gemspec
+++ snack.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'snack'
-  s.version     = '0.2.2'
+  s.version     = '0.2.3'
   s.date        = '2013-09-13'
   s.summary     = 'A static website framework.'
   s.description = 'Snack is a small framework for building static websites.'
@@ -23,6 +23,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'sass'
   s.add_development_dependency 'minitest', '~> 5'
   s.add_development_dependency 'rack-test'
-  # s.add_development_dependency 'coveralls'
+  s.add_development_dependency 'coveralls'
   s.add_development_dependency 'rake'
 end
--- test/snack_test.rb
+++ test/snack_test.rb
@@ -3,8 +3,11 @@
 require File.expand_path('../../lib/snack', __FILE__)
 require 'minitest/autorun'
 require 'rack/test'
+require 'coveralls'
 
-MiniTest::Unit.autorun
+Coveralls.wear!
+
+Minitest.autorun
 
 describe Snack::Server do
   include Rack::Test::Methods
@@ -14,28 +17,28 @@ describe Snack::Server do
   end
 
   it 'should respond with 404 on bad requests' do
-    get('/foo')
+    get '/foo'
     last_response.status.must_equal 404
   end
 
   it 'should serve static file directly first' do
-    get('/public/style.css')
+    get '/public/style.css'
     last_response.body.must_equal "body {\n\tbackground: blue;\n}"
   end
 
   it 'should compile and serve sass files' do
-    get('/public/sass_style.css')
+    get '/public/sass_style.css'
     last_response.body.must_equal "body {\n  background: blue; }\n"
   end
 
   it 'should serve coffeescript files compiled through tilt if request path exists with coffee extension' do
-    get('/public/application.js')
+    get '/public/application.js'
     last_response.body.must_equal "(function() {\n  $(document).ready(function() {\n    return alert('hello from snack');\n  });\n\n}).call(this);\n"
   end
 
   it 'should default to index.html if directory is requested' do
-    direct = get('/index.html')
-    response = get('/')
+    direct = get '/index.html'
+    response = get '/'
 
     last_response.status.must_equal direct.status
     last_response.body.must_equal direct.body
@@ -43,29 +46,29 @@ describe Snack::Server do
 
   # partials
   it 'should render partials if found' do
-    get('/pages/partial-normal.html')
+    get '/pages/partial-normal.html'
     last_response.body.must_equal "This is a partial!\n"
   end
 
   it 'should error if partial not found' do
-    get('/pages/partial-failing.html')
+    get '/pages/partial-failing.html'
     last_response.status.must_equal 500
   end
 
   # layouts
   it 'should render a page within a layout if layout exists' do
-    get('/pages/layout-normal.html')
+    get '/pages/layout-normal.html'
     last_response.body.must_equal "<div id='content'>\n  <h1>Page Content</h1>\n</div>\n"
   end
 
   it 'should error if user set layout and layout not found' do
-    get('/pages/layout-failing.html')
+    get '/pages/layout-failing.html'
     last_response.status.must_equal 500
   end
 
   # variables
   it 'should pass variables defined in the page to the layout' do
-    get('/pages/variable-normal.html')
+    get '/pages/variable-normal.html'
     last_response.body.must_equal "happy\nsad\nmad\n"
   end
 
